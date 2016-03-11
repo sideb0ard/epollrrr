3e4c1109acb463d4ffe9c467317499e4143a3f2f
Thor Sideburns <sideboard@google.com>
2016-03-11 11:30:48 -0800

    added timerfd

--- gbpollerrr.cpp
+++ gbpollerrr.cpp
@@ -4,23 +4,27 @@
 #include <stdlib.h>
 #include <sys/epoll.h>
 #include <sys/signalfd.h>
+#include <sys/timerfd.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <iostream>
+#include <vector>
 
 #define ARRAY_SIZE(ra) (sizeof(ra)/sizeof(ra[0]))
 #define MAXEVENTS 64
 #define handle_error(msg) \
-  do { perror(msg); exit(EXIT_FAILURE); } while (0)
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+struct fd_callback_data {
+    int fd;
+    epoll_event data;
+};
+
 
 int epfd = -1;
 int fd_signal = -1;
-
-struct callback_wrapper {
-    // as far as I can tell, no way to cast a data pointer to
-    // a function pointer, so using a wrapper instead
-    void (*fire)();
-};
+int fd_timer = -1;
 
 void on_signal()
 {
@@ -30,8 +34,7 @@ void on_signal()
     ssize_t bytes = read(fd_signal, &info, sizeof(info));
     assert(bytes == sizeof(info));
 }
-
-int setup_fd_signal()
+struct fd_callback_data setup_signalfd()
 {
     sigset_t mask;
     sigemptyset ( &mask );
@@ -39,44 +42,79 @@ int setup_fd_signal()
 
     // block default signal handler
     if ( sigprocmask ( SIG_BLOCK, &mask, NULL ) == 1 )
-      handle_error("sigprocmask");
+        handle_error("sigprocmask");
 
-    int fd_signal = signalfd ( -1, &mask, 0 );
+    fd_signal = signalfd ( -1, &mask, 0 );
     if ( fd_signal == -1 )
-      handle_error ( "signalfd" );
+        handle_error ( "signalfd" );
 
-    return fd_signal;
+    epoll_event fd_signal_data = { EPOLLIN, { .ptr = (void*) on_signal }};
+
+    return (struct fd_callback_data){ fd_signal, fd_signal_data };
+
 }
 
- 
-  
+void on_stdin()
+{
+    std::cout << "Boo ya: STDIN Input\n";
+}
+
+void on_timer_up()
+{
+    std::cout << "Boom, timer expired!\n";
+    uint64_t exp;
+    ssize_t s = read(fd_timer, &exp, sizeof(uint64_t));
+    assert(s == sizeof(uint64_t));
+}
+struct fd_callback_data setup_timerfd()
+{
+
+    struct timespec now;
+    if (clock_gettime(CLOCK_REALTIME, &now) == -1)
+        handle_error("clock_gettime");
+
+    struct itimerspec new_value;
+    new_value.it_value.tv_sec = now.tv_sec + 3;
+    new_value.it_value.tv_nsec = now.tv_nsec;
+    new_value.it_interval.tv_nsec = 0;
+
+    fd_timer = timerfd_create(CLOCK_REALTIME, 0);
+    if (fd_timer == -1)
+        handle_error("timerfd_create");
+
+    if (timerfd_settime(fd_timer, TFD_TIMER_ABSTIME, &new_value, NULL) == -1)
+        handle_error("timerfd_settime");
+
+    epoll_event fd_timer_data = { EPOLLIN, { .ptr = (void*) on_timer_up }};
+
+    return (struct fd_callback_data){ fd_timer, fd_timer_data };
+}
+
+
 int main(int argc, char* argv[])
 {
+    std::vector<fd_callback_data> polls;
+    polls.push_back(setup_signalfd());
+    polls.push_back(setup_timerfd());
+
     epfd = epoll_create ( 1 );
-    int ret = 0;
 
-    fd_signal = setup_fd_signal();
-    struct callback_wrapper* cb;
-    cb = (struct callback_wrapper *) calloc(1, sizeof(struct callback_wrapper));
-    cb->fire = &on_signal;
-    epoll_event fd_signal_data = { EPOLLIN, { .ptr = cb }};
-
-    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, fd_signal, &fd_signal_data);
-    if (ret)
-      handle_error("epoll_ctl");
+    for (auto poll : polls) {
+        int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, poll.fd, &poll.data);
+        if (ret)
+            handle_error("epoll_ctl");
+    }
 
     epoll_event events[MAXEVENTS];
     int event_count;
     for (;;) {
         event_count = epoll_wait(epfd, events, MAXEVENTS, -1);
         if ( event_count < 0 )
-          handle_error("epoll_wait");
+            handle_error("epoll_wait");
 
         for ( unsigned n = 0 ; n < event_count ; ++n ) {
-            auto callback = static_cast<struct callback_wrapper*>(events[n].data.ptr)->fire;
+            auto callback = reinterpret_cast<void (*)()>(events[n].data.ptr);
             callback();
         }
     }
-
-    return 0;
 }
